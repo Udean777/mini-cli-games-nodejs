4bc5918ddd72b245bc3a758d01f244b3f0d7aded
Udean777 <uaart123@gmail.com>
2023-09-13 13:56:43 +0700

    nambahin fitur nyawa di game pertama

--- game.js
+++ game.js
@@ -4,6 +4,7 @@ class GameTebakAngka {
   constructor() {
     this.angkaDitebak = Math.floor(Math.random() * 100) + 1;
     this.percobaan = 0;
+    this.nyawa = 3;
   }
 
   play() {
@@ -12,7 +13,7 @@ class GameTebakAngka {
       "Saya telah memilih sebuah angka antara 1 hingga 100. Coba tebak angka tersebut."
     );
 
-    while (true) {
+    while (this.nyawa > 0) {
       const tebak = parseInt(readline.question("Masukkan tebakan Anda: "));
 
       if (isNaN(tebak) || tebak < 1 || tebak > 100) {
@@ -31,6 +32,16 @@ class GameTebakAngka {
         console.log("Angka yang Anda tebak terlalu kecil. Coba lagi.");
       } else {
         console.log("Angka yang Anda tebak terlalu besar. Coba lagi.");
+      }
+
+      this.nyawa--;
+
+      if (this.nyawa === 0) {
+        console.log("Anda kehabisan nyawa. Coba lagi untuk memulai");
+        console.log(`Angka yang benar adalah: ${this.angkaDitebak}`);
+        break;
+      } else {
+        console.log(`Sisa nyawa: ${this.nyawa}`);
       }
     }
   }
